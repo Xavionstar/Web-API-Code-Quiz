acd1e2e7cca6f93e6590382f6db6da77b890dd2b
Xavionstar <lincpough@gmail.com>
2022-12-04 14:36:25 -0800

    Added the correct and wrong messages on click of buttons

--- assets/script.js
+++ assets/script.js
@@ -4,12 +4,17 @@ var pageThree = document.getElementById("pageThree");
 var finalScorePage = document.getElementById("finalScorePage");
 var highScorePage = document.getElementById("highScorePage");
 var startQuizButton = document.getElementById("startQuizButton");
-var wrongAnswer = document.querySelectorAll(".wrongAnswer")
+var wrongAnswerButtons = document.querySelectorAll(".wrongAnswer")
 var pageTwoAnswer = document.getElementById("pageTwoAnswer")
 var pageThreeAnswer = document.getElementById("pageThreeAnswer")
 var submitButton = document.getElementById("submitButton")
 var goBack = document.getElementById("goBack")
 var highScoreLink = document.getElementById("highScoreLink")
+var correctAnswerAlertOne = document.getElementById("correctAnswerAlertOne")
+var wrongAnswerAlertOne = document.getElementById("wrongAnswerAlertOne")
+var correctAnswerAlertTwo = document.getElementById("correctAnswerAlertTwo")
+var wrongAnswerAlertTwo = document.getElementById("wrongAnswerAlertTwo")
+
 
 startQuizButton.addEventListener("click", function(){
     pageOne.classList.add('hide');
@@ -17,13 +22,21 @@ startQuizButton.addEventListener("click", function(){
 });
 
 pageTwoAnswer.addEventListener("click", function(){
-    pageTwo.classList.add('hide');
-    pageThree.classList.remove('hide');
+    correctAnswerAlertOne.classList.remove("hide");
+    setTimeout(function(){
+        pageTwo.classList.add('hide');
+        pageThree.classList.remove('hide');
+        correctAnswerAlertOne.classList.add("hide")
+    }, 1500)
 });
 
 pageThreeAnswer.addEventListener("click", function(){
-    pageThree.classList.add('hide');
-    finalScorePage.classList.remove('hide');
+    correctAnswerAlertTwo.classList.remove("hide");
+    setTimeout(function(){
+        pageThree.classList.add('hide');
+        finalScorePage.classList.remove('hide');
+        correctAnswerAlertTwo.classList.add("hide")
+    }, 1500)
 });
 
 submitButton.addEventListener("click", function(){
@@ -43,3 +56,16 @@ highScoreLink.addEventListener("click", function(){
     finalScorePage.classList.add('hide');
     highScorePage.classList.remove('hide');
 });
+
+wrongAnswerButtons.forEach(wrongAnswerButton => {
+    wrongAnswerButton.addEventListener('click', () => {
+    wrongAnswerAlertOne.classList.remove("hide")
+    wrongAnswerAlertTwo.classList.remove("hide")
+    setTimeout(function(){
+        wrongAnswerAlertOne.classList.add("hide")
+        wrongAnswerAlertTwo.classList.add("hide")
+    }, 1500)
+    })
+});
+
+
